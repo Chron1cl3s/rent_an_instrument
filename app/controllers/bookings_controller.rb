0bd9f211f901c6618b77643b9bbc9b0db275e18f
Markus Wahrheit <m.wahrheit@outlook.com>
2020-05-27 16:30:46 +0200

    latest version

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,49 +1,48 @@
-class BookingsController < ApplicationController
-end
+# class BookingsController < ApplicationController
 
-def create
-    @instrument = Instrument.find(params[:instrument_id])
-    @booking = Booking.new(booking_params)
-    @booking.instrument = @instrument
-    @booking.user = current_user
-    @booking.status = "Pending"
-    if @booking.save
-      redirect_to booking_path(@booking)
-    else
-      redirect_to instrument_path(@instrument)
-    end
-  end
+# def create
+#     @instrument = Instrument.find(params[:instrument_id])
+#     @booking = Booking.new(booking_params)
+#     @booking.instrument = @instrument
+#     @booking.user = current_user
+#     @booking.status = "Pending"
+#     if @booking.save
+#       redirect_to booking_path(@booking)
+#     else
+#       redirect_to instrument_path(@instrument)
+#     end
+#   end
 
-  def index
-    @instruments = Instrument.where(user_id: current_user.id)
-  end
+#   def index
+#     @instruments = Instrument.where(user_id: current_user.id)
+#   end
 
-  def show
-    set_booking
-    @instrument = @booking.instrument
-  end
+#   def show
+#     set_booking
+#     @instrument = @booking.instrument
+#   end
 
-  def update
-    set_booking
-    @booking.status = "Pending"
-    @booking.save!
-    redirect_to booking_path(@booking)
-  end
+#   def update
+#     set_booking
+#     @booking.status = "Pending"
+#     @booking.save!
+#     redirect_to booking_path(@booking)
+#   end
 
-  def destroy
-    set_booking
-    @booking.destroy
-    redirect_to root_path
-  end
+#   def destroy
+#     set_booking
+#     @booking.destroy
+#     redirect_to root_path
+#   end
 
-  private
+#   private
 
-  def booking_params
-    params.require(:booking).permit(:status)
-  end
+#   def booking_params
+#     params.require(:booking).permit(:status)
+#   end
 
-  def set_booking
-    @booking = Booking.find(params[:id])
-  end
+#   def set_booking
+#     @booking = Booking.find(params[:id])
+#   end
 
-end
+# end
