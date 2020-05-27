6ca21cb371d6dc5f9f095612782399aa311a95f9
Markus Wahrheit <m.wahrheit@outlook.com>
2020-05-27 14:07:20 +0200

    Login in and Sign Up done

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -30,11 +30,3 @@ class Users::SessionsController < Devise::SessionsController
   # end
 end
 
-
-# def create
-#     self.resource = warden.authenticate!(auth_options)
-#     set_flash_message!(:notice, :signed_in)
-#     sign_in(resource_name, resource)
-#     yield resource if block_given?
-#     respond_with resource, location: after_sign_in_path_for(resource)
-#   end
