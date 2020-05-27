57673b124cce863c786e3ee0a89c4f98f930759c
leon kranich <leon_kranich@yahoo.de>
2020-05-27 21:15:00 +0200

    link now working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_100339) do
+ActiveRecord::Schema.define(version: 2020_05_27_114846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_100339) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "photo"
+    t.string "photo_s"
     t.index ["user_id"], name: "index_instruments_on_user_id"
   end
 
